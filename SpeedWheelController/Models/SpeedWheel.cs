7014048bae757256e1a748501349285b67c29739
ninebelow <speedwheel@ninebelow.com>
2023-11-07 01:01:01 -0600

    Remove calibration and simplify things by specifying 90 degrees value

--- SpeedWheelController/Models/SpeedWheel.cs
+++ SpeedWheelController/Models/SpeedWheel.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.ObjectModel;
 using System.Linq;
-using System.Windows.Media;
 using System.Windows.Threading;
 using Nefarius.ViGEm.Client;
 using Nefarius.ViGEm.Client.Targets;
@@ -13,58 +11,45 @@ namespace SpeedWheelController.Models
 {
     public class SpeedWheel : BaseModel
     {
-        // Each of the thumbstick axis members is a signed value between -32768 and 32767 describing the position of the thumbstick.
-        // A value of 0 is centered. Negative values signify down or to the left.
-        // Positive values signify up or to the right.
-
-        private int steeringMaximumValue = 32_767;
-
-        private int steeringMinimumValue = -32_768;
-
         private int steeringValue = 0;
 
-        private int accelerationMaximumValue = 255;
-
-        private int accelerationMinimumValue = 0;
-
         private int accelerationValue = 0;
-
-        private int brakingMaximumValue = 255;
-
-        private int brakingMinimumValue = 0;
-
+        
         private int brakingValue = 0;
-
-        private int motorSpeedMaximumValue = 65_535;
-
-        private int motorSpeedMinimumValue = 0;
 
         private int leftMotorSpeedValue = 0;
 
         private int rightMotorSpeedValue = 0;
 
-        private double steeringGrowth = 1.0;
-
-        private double? steeringLeft90Value = null;
-
-        private double? steeringRight90Value = null;
-
-        private double? steeringCenteredValue = null;
-
         private string message = string.Empty;
-
-        private bool calibrated = false;
-
-        private bool aButtonWasReleased = true;
 
         private IXbox360Controller? virtualController;
 
         private DispatcherTimer timer = new DispatcherTimer();
 
         private Controller? physicalController;
+
         private State? previousState;
 
-        public TimeSpan PollInterval => TimeSpan.FromMilliseconds(10);
+        private int ninetyDegrees = 25_000;
+
+        private TimeSpan pollInterval = TimeSpan.FromMilliseconds(10);
+
+        public int SteeringMaximumValue => 32_767;
+
+        public int SteeringMinimumValue => -32_768;
+
+        public int AccelerationMaximumValue => 255;
+
+        public int AccelerationMinimumValue => 0;
+
+        public int BrakingMaximumValue => 255;
+
+        public int BrakingMinimumValue => 0;
+
+        public int MotorSpeedMaximumValue => 65_535;
+
+        public int MotorSpeedMinimumValue => 0;
 
         public int SteeringValue
         {
@@ -84,112 +69,6 @@ namespace SpeedWheelController.Models
             }
         }
 
-        public int SteeringMaximumValue
-        {
-            get
-            {
-                return this.steeringMaximumValue;
-            }
-            set
-            {
-                if (value == this.steeringMaximumValue)
-                {
-                    return;
-                }
-
-                this.steeringMaximumValue = value;
-                base.OnPropertyChanged(nameof(this.SteeringMaximumValue));
-            }
-        }
-
-        public int SteeringMinimumValue
-        {
-            get
-            {
-                return this.steeringMinimumValue;
-            }
-            set
-            {
-                if (value == this.steeringMinimumValue)
-                {
-                    return;
-                }
-
-                this.steeringMinimumValue = value;
-                base.OnPropertyChanged(nameof(this.SteeringMinimumValue));
-            }
-        }
-
-        public double? SteeringLeft90Value
-        {
-            get
-            {
-                return this.steeringLeft90Value;
-            }
-            set
-            {
-                if (value == this.steeringLeft90Value)
-                {
-                    return;
-                }
-
-                this.steeringLeft90Value = value;
-                base.OnPropertyChanged(nameof(this.SteeringLeft90Value));
-            }
-        }
-
-        public double? SteeringRight90Value
-        {
-            get
-            {
-                return this.steeringRight90Value;
-            }
-            set
-            {
-                if (value == this.steeringRight90Value)
-                {
-                    return;
-                }
-
-                this.steeringRight90Value = value;
-                base.OnPropertyChanged(nameof(this.SteeringRight90Value));
-            }
-        }
-
-        public double? SteeringCenteredValue
-        {
-            get
-            {
-                return this.steeringCenteredValue;
-            }
-            set
-            {
-                if (value == this.steeringCenteredValue)
-                {
-                    return;
-                }
-
-                this.steeringCenteredValue = value;
-                base.OnPropertyChanged(nameof(this.SteeringCenteredValue));
-            }
-        }
-
-        public DoubleCollection SteeringTicks
-        {
-            get
-            {
-                return new DoubleCollection
-                {
-                    this.SteeringMinimumValue,
-                    this.SteeringLeft90Value ?? 0,
-                    this.SteeringCenteredValue ?? 0,
-                    this.SteeringRight90Value ?? 0,
-                    this.SteeringMaximumValue
-                };
-            }
-        }
-
-
         public int AccelerationValue
         {
             get
@@ -205,42 +84,6 @@ namespace SpeedWheelController.Models
 
                 this.accelerationValue = value;
                 base.OnPropertyChanged(nameof(this.AccelerationValue));
-            }
-        }
-
-        public int AccelerationMaximumValue
-        {
-            get
-            {
-                return this.accelerationMaximumValue;
-            }
-            set
-            {
-                if (value == this.accelerationMaximumValue)
-                {
-                    return;
-                }
-
-                this.accelerationMaximumValue = value;
-                base.OnPropertyChanged(nameof(this.AccelerationMaximumValue));
-            }
-        }
-
-        public int AccelerationMinimumValue
-        {
-            get
-            {
-                return this.accelerationMinimumValue;
-            }
-            set
-            {
-                if (value == this.accelerationMinimumValue)
-                {
-                    return;
-                }
-
-                this.accelerationMinimumValue = value;
-                base.OnPropertyChanged(nameof(this.AccelerationMinimumValue));
             }
         }
 
@@ -261,46 +104,6 @@ namespace SpeedWheelController.Models
                 base.OnPropertyChanged(nameof(this.BrakingValue));
             }
         }
-
-        public int BrakingMaximumValue
-        {
-            get
-            {
-                return this.brakingMaximumValue;
-            }
-            set
-            {
-                if (value == this.brakingMaximumValue)
-                {
-                    return;
-                }
-
-                this.brakingMaximumValue = value;
-                base.OnPropertyChanged(nameof(this.BrakingMaximumValue));
-            }
-        }
-
-        public int BrakingMinimumValue
-        {
-            get
-            {
-                return this.brakingMinimumValue;
-            }
-            set
-            {
-                if (value == this.brakingMinimumValue)
-                {
-                    return;
-                }
-
-                this.brakingMinimumValue = value;
-                base.OnPropertyChanged(nameof(this.BrakingMinimumValue));
-            }
-        }
-
-        public int MotorSpeedMaximumValue => this.motorSpeedMaximumValue;
-
-        public int MotorSpeedMinimumValue => this.motorSpeedMinimumValue;
 
         public int LeftMotorSpeedValue
         {
@@ -355,7 +158,7 @@ namespace SpeedWheelController.Models
         {
             this.Message = "Starting!";
             
-            this.timer.Interval = this.PollInterval;
+            this.timer.Interval = this.pollInterval;
             this.timer.Tick += new EventHandler(this.ConfigureSpeedWheel);
             this.timer.Start();
         }
@@ -401,7 +204,7 @@ namespace SpeedWheelController.Models
             }
 
             this.LeftMotorSpeedValue = (ushort)(((decimal)e.LargeMotor / 255m) * this.MotorSpeedMaximumValue);
-            this.RightMotorSpeedValue = (ushort)(((decimal)e.SmallMotor / 255m) * this.motorSpeedMaximumValue);
+            this.RightMotorSpeedValue = (ushort)(((decimal)e.SmallMotor / 255m) * this.MotorSpeedMaximumValue);
         }
 
         private void SetVibration()
@@ -450,26 +253,16 @@ namespace SpeedWheelController.Models
                         this.virtualController?.SetButtonState(Xbox360Button.LeftShoulder, false);
                     }
 
-                    // Each of the thumbstick axis members is a signed value between -32768 and 32767 describing the position of the thumbstick.
-                    // A value of 0 is centered. Negative values signify down or to the left.
-                    // Positive values signify up or to the right.
-                    // The constants XINPUT_GAMEPAD_LEFT_THUMB_DEADZONE or XINPUT_GAMEPAD_RIGHT_THUMB_DEADZONE can be used as a positive and negative value to filter a thumbstick input.
-                    // Left thumbstick x-axis value.
-
                     double steeringIn = (double)(state?.Gamepad.LeftThumbX ?? 0);
                     double steeringOut = steeringIn;
-
-                    if (this.calibrated)
+                  
+                    if (steeringIn > 0)
                     {
-                        steeringIn = steeringIn - (double?)this.SteeringCenteredValue ?? 0.0;
-                        if (steeringIn > 0)
-                        {
-                            steeringOut = Math.Min(steeringIn / (double)(this.SteeringRight90Value ?? this.SteeringMaximumValue), 1.0) * this.SteeringMaximumValue;
-                        }
-                        if (steeringIn < 0)
-                        {
-                            steeringOut = -1 * (Math.Max(-steeringIn / (double)(this.SteeringLeft90Value ?? this.SteeringMinimumValue), -1.0) * this.SteeringMinimumValue);
-                        }
+                        steeringOut = Math.Min(steeringIn, this.ninetyDegrees) / this.ninetyDegrees * this.SteeringMaximumValue;
+                    }
+                    else if (steeringIn < 0)
+                    {
+                        steeringOut = Math.Min(-steeringIn, this.ninetyDegrees) / this.ninetyDegrees * this.SteeringMinimumValue;
                     }
 
                     this.SteeringValue = (int)steeringOut;
@@ -505,17 +298,6 @@ namespace SpeedWheelController.Models
                     this.virtualController.SubmitReport();
 
                     this.previousState = state;
-
-                    // calibration
-                    if (!this.IsButtonPressed(state, GamepadButtonFlags.A))
-                    {
-                        this.aButtonWasReleased = true;
-                    }
-
-                    if (!this.calibrated && this.aButtonWasReleased)
-                    {
-                        this.Calibrate(this.IsButtonPressed(state, GamepadButtonFlags.A));
-                    }
                 }
             }
             catch
@@ -549,47 +331,7 @@ namespace SpeedWheelController.Models
             }
             else
             {
-                this.Calibrate();
-            }
-        }
-
-        private void Calibrate(bool confirmButtonPressed = false)
-        {
-            if (this.calibrated)
-            {
-                return;
-            }
-
-            if (this.SteeringLeft90Value == null)
-            {
-                this.Message = "Please turn wheel 90 degrees to the left and press A button.";
-                if (confirmButtonPressed)
-                {
-                    this.SteeringLeft90Value = this.SteeringValue;
-                }
-            }
-            else if (this.SteeringRight90Value == null)
-            {
-                this.Message = "Please turn wheel 90 degrees to the right and press A button.";
-                if (confirmButtonPressed)
-                {
-                    this.SteeringRight90Value = this.SteeringValue;
-                }
-            }
-            else if (this.SteeringCenteredValue == null)
-            {
-                this.Message = "Please turn wheel to center and press A button.";
-                if (confirmButtonPressed)
-                {
-                    this.SteeringCenteredValue = this.SteeringValue;
-                    this.Message = "SpeedWheel connected as Xbox 360 controller and calibrated!";
-                    this.calibrated = true;
-                }
-            }
-
-            if (confirmButtonPressed)
-            {
-                this.aButtonWasReleased = false;
+                this.Message = "SpeedWheel connected as an Xbox 360 controller... ready to race!";
             }
         }
     }
